f9e796f1ade5f15908ea254d426235c5f8e5e27e
koocooplatter <schwarz.kyle0@gmail.com>
2016-11-11 12:12:57 -0500

    Add files via upload

---
+++ todo.cpp
@@ -0,0 +1,39 @@
+/////////////////////
+//	Final Project  //
+/////////////////////
+
+/*Not allowed to use STL library*/
+
+
+//Implement mining algorithm Apriori:
+
+
+//Part 1:
+	- Assign tasks to team members
+	- Each team member explains how they plan to accomplish goals
+
+//Part 2:
+	- Implement Apriori & apply to titanic dataset
+		- # of association rules w/ minimum support = 0.75
+		- # of rules with confidence = 1
+		- Support/Confidence of survival rate of 1st, 2nd, 3rd class children
+		- Did children in 2nd class have better survival chance than 1st class children?
+		- Did 1st & 2nd class children have better survival chance than 3rd class children?
+		- Do you get more or less if lower minimum support val? Why?
+		- Give example of a redundant rule
+		- Present results with one slide per question (at least 2 slides for apriori algorithm)
+		
+//Part 3:
+	- How to apply an algorithm similar to apriori but in the other direction?
+	
+	
+	
+//////////////
+//Team Tasks//
+//////////////
+Kyle:
+	- Confidence/Min Support
+Curtis:
+	- File I/O
+Kyle & Curtis:
+	- Apriori
